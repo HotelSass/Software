49711ed50c65d8258cf330805bc64dce3fa44089
sidghimire <siddharthaghimire@gmail.com>
2023-07-29 22:52:08 +0545

    message

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -3,7 +3,7 @@ import { BiX } from "react-icons/bi";
 type ModalProps = {
   open: boolean;
   width: number;
-  height:number;
+  height: number;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>
 };
 const Modal = ({
@@ -17,16 +17,18 @@ const Modal = ({
     return <></>;
   }
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50 ">
-      <div className="bg-white rounded-md p-4 " style={{ width: width, height: height == null ? 'auto' : height,overflowY:height == null ? 'auto' :'scroll' }}>
-        <div className="w-full">
-          <button className="w-full flex justify-end p-3 " onClick={() => setOpen(false)}>
-            <BiX size={24} />
-          </button>
+    <>
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50 ">
+        <div className="bg-white rounded-md p-4 " style={{ width: width, height: height == null ? 'auto' : height, overflowY: height == null ? 'auto' : 'scroll' }}>
+          <div className="w-full">
+            <button className="w-full flex justify-end p-3 " onClick={() => setOpen(false)}>
+              <BiX size={24} />
+            </button>
+          </div>
+          {children}
         </div>
-        {children}
       </div>
-    </div>
+    </>
   );
 };
 
