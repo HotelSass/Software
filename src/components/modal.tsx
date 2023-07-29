a1c306728455e2ea0d669d2f0d8ecaca60263ec2
sidghimire <siddharthaghimire@gmail.com>
2023-07-29 23:30:07 +0545

    message

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -13,21 +13,21 @@ function Modal({
   height,
   setOpen
 }: { children: React.ReactNode } & ModalProps) {
-  if (open == false) {
-    return null;
-  }
+
   return (
     <>
-      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50 ">
-        <div className="bg-white rounded-md p-4 " style={{ width: width, height:'auto' }}>
-          <div className="w-full">
-            <button className="w-full flex justify-end p-3 " onClick={() => setOpen(false)}>
-              <BiX size={24} />
-            </button>
+      {open == false ? null :
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50 ">
+          <div className="bg-white rounded-md p-4 " style={{ width: width, height: 'auto' }}>
+            <div className="w-full">
+              <button className="w-full flex justify-end p-3 " onClick={() => setOpen(false)}>
+                <BiX size={24} />
+              </button>
+            </div>
+            {children}
           </div>
-          {children}
         </div>
-      </div>
+      }
     </>
   );
 };
