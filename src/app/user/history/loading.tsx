203d09bbc8c78d525dbc41cccc8d9adec702d5b3
sidghimire <siddharthaghimire@gmail.com>
2023-09-06 16:10:04 +0545

    Added Corrected skeleton

--- src/app/user/history/loading.tsx
+++ src/app/user/history/loading.tsx
@@ -30,41 +30,44 @@ const Rooms = () => {
 
 
                 </div>
-                <div className=" flex flex-row">
 
-                    <div role="status" className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
-
-                        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-                        <div className="flex items-center mt-4 space-x-3">
-
-                            <div>
-                                <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
-                                <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-                            </div>
+                <div role="status" className="w-full h-1/2 p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700 mt-5">
+                    <div className="flex items-center justify-between">
+                        <div>
+                            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+                            <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                         </div>
-                        <span className="sr-only">Loading...</span>
+                        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
                     </div>
-                    <div role="status" className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
-
-                        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-                        <div className="flex items-center mt-4 space-x-3">
-
-                            <div>
-                                <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
-                                <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-                            </div>
+                    <div className="flex items-center justify-between pt-4">
+                        <div>
+                            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+                            <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                         </div>
-                        <span className="sr-only">Loading...</span>
+                        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
                     </div>
-
-
-
+                    <div className="flex items-center justify-between pt-4">
+                        <div>
+                            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+                            <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                        </div>
+                        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                    </div>
+                    <div className="flex items-center justify-between pt-4">
+                        <div>
+                            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+                            <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                        </div>
+                        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                    </div>
+                    <div className="flex items-center justify-between pt-4">
+                        <div>
+                            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+                            <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                        </div>
+                        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+                    </div>
+                    <span className="sr-only">Loading...</span>
                 </div>
 
             </div>
